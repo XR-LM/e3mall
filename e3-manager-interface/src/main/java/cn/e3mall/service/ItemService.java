b260c974cb21a434796783849c33cbe9d94fff24
XR-LM <1595242796@qq.com>
2018-12-27 22:07:53 +0800

    dubbo部署，实现查询所有商品

--- e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
+++ e3-manager-interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -1,10 +1,18 @@
 package cn.e3mall.service;
 
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.page.EasyUIDataGridResult;
 
 public interface ItemService {
 	/*
 	 * 根据id查询商品信息
 	 */
 	TbItem getItemById(long id) ;
+	/**
+	 * 分页查询
+	 * @param page
+	 * @param rows
+	 * @return
+	 */
+	EasyUIDataGridResult getItemList(int page, int rows);
 }
--- e3-manager-service/src/main/java/cn/e3mall/service/ItemServiceImpl.java
+++ e3-manager-service/src/main/java/cn/e3mall/service/ItemServiceImpl.java
@@ -1,10 +1,17 @@
 package cn.e3mall.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
+import cn.e3mall.pojo.page.EasyUIDataGridResult;
 
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -21,6 +28,24 @@ public class ItemServiceImpl implements ItemService {
 		// TODO 自动生成的方法存根
 		TbItem item= itemMapper.selectByPrimaryKey(id);
 		return item;
+	}
+
+	@Override
+	public EasyUIDataGridResult getItemList(int page, int rows) {
+		// 从page页开始，每页rows条数据,该方法会自动查询所有数据
+		PageHelper.startPage(page, rows);
+		//设置查询条件
+		TbItemExample example = new TbItemExample();
+		//默认查询所有
+		List<TbItem> list=itemMapper.selectByExample(example);
+		//取分页信息
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		//创建返回结果对象
+		EasyUIDataGridResult result = new EasyUIDataGridResult();
+		result.setTotal((int) pageInfo.getTotal());
+		result.setRows(list);
+		
+		return result;
 	}
 
 }
