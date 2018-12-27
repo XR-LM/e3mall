1b41a39fbecd3d874053827d5cd293f734a1bbb3
XR-LM <1595242796@qq.com>
2018-12-27 22:24:19 +0800

    添加分类辅助类

---
+++ src/main/java/cn/e3mall/pojo/page/EasyUIDataGridResult.java
@@ -0,0 +1,38 @@
+package cn.e3mall.pojo.page;
+
+import java.io.Serializable;
+import java.util.List;
+/**
+ * 翻页辅助类
+ * @author XR
+ *
+ */
+public class EasyUIDataGridResult implements Serializable {
+
+    private Integer total;
+
+    private List<?> rows;
+
+    public EasyUIDataGridResult(Integer total, List<?> rows) {
+        this.total = total;
+        this.rows = rows;
+    }
+    public EasyUIDataGridResult(){}
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public List<?> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<?> rows) {
+        this.rows = rows;
+    }
+
+}
